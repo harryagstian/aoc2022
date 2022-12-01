95f54cd118c885025f629bd31427201fbb1066a5
Harry Agustian <harry.agustian@autobahn-security.com>
2022-12-01 13:19:30 +0700

    Simplify day 1 solution

--- src/day01/solution.rs
+++ src/day01/solution.rs
@@ -6,34 +6,26 @@ pub fn main() {
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let mut counter = 0;
-    let mut highest_value = 0;
     let mut current_value = 0;
     let mut value_stacks: Vec<i32> = Vec::new();
 
     for line in contents.lines() {
         if line == "" {
-            if current_value > highest_value {
-                highest_value = current_value;
-            }
             value_stacks.push(current_value);
             current_value = 0;
-            counter = counter + 1;
         } else {
             let value: i32 = line.parse().unwrap();
             current_value = current_value + value;
         }
     }
 
-    if current_value > highest_value {
-        highest_value = current_value;
-    }
     value_stacks.push(current_value);
-    counter = counter + 1;
-
-    println!("{} {}", counter, highest_value);
     value_stacks.sort();
     value_stacks.reverse();
-    println!("{:?}", value_stacks);
-    println!("{}", value_stacks[0] + value_stacks[1] + value_stacks[2]);
+
+    let part1 = value_stacks[0];
+    let part2 = value_stacks[0] + value_stacks[1] + value_stacks[2];
+
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
